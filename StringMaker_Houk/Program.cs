6eca79fb0703a5c4b4c8535ddfc6f9620d673707
Lee Houk <leehouk@gmail.com>
2023-10-17 20:58:46 -0700

    Add project files.

---
+++ StringMaker_Houk/Program.cs
@@ -0,0 +1,40 @@
+﻿// Lee Houk
+// IT113
+// NOTES: Fun project, really got me thinking.
+// BEHAVIORS NOT IMPLIMENTED AND WHY: Got them all
+
+namespace StringMaker_Houk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringManager sm = new StringManager();
+
+            Console.WriteLine("Reverse string");
+            string reverse = "Sunbeam Tiger";
+            Console.WriteLine(sm.Reverse(reverse) + "\n");
+
+            Console.WriteLine("Convert a string to ASCII and return the summed value as words");
+            string strAscii = "One Two Five Four";
+            sm.SetInput(strAscii);
+            Console.WriteLine(sm.ToString() + "\n");
+
+            Console.WriteLine("Reverse string with preserved case location");
+            Console.WriteLine(sm.Reverse(reverse, true) + "\n");
+
+            Console.WriteLine("Equality check");
+            string equality = "Sunbeam Tiger";
+            sm.SetInput(equality);
+            Console.WriteLine(sm.Equals("Sunbeam Tiger") + "\n");
+
+            Console.WriteLine("Symmetric string");
+            string sym1 = "ABBA";
+            string sym2 = "ABA";
+            string sym3 = "ABba";
+            Console.WriteLine(sm.Symmetric(sym1));
+            Console.WriteLine(sm.Symmetric(sym2));
+            Console.WriteLine(sm.Symmetric(sym3) + "\n");
+        }
+    }
+}
---
+++ StringMaker_Houk/StringManager.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringMaker_Houk
+{
+    internal class StringManager
+    {
+        public StringManager()
+        {
+        }
+
+        // Input a string and return the reverse of the string
+        public string Reverse(string s)
+        {
+            string reverse = "";
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+
+                reverse += s[i];
+            }
+            return reverse;
+        }
+
+        // Input a string and return the reverse, keeping the case of
+        // the letters by location
+        public string Reverse(string s, bool PreserveCaseLocation)
+        {
+            string reverse = "";
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                if (Char.IsUpper(s[s.Length - 1 - i]))
+                    reverse += s[i].ToString().ToUpper();
+                else
+                    reverse += s[i].ToString().ToLower();
+            }
+            return reverse;
+        }
+
+        // Input a string and return true if the string is symmetric
+        public bool Symmetric(string s)
+        {
+            string reverse = "";
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                reverse += s[i];
+            }
+            if (s == reverse)
+                return true;
+            else
+                return false;
+        }
+
+        // Variable to hold the input string to be fed into the ToString
+        // method
+        private string _input = "";
+
+        // Set the input string
+        public void SetInput(string s)
+        {
+            _input = s;
+        }
+
+        // Return the string as a number
+        public override string ToString()
+        {
+            int sum = 0;
+
+            // If the input string is null or empty, return "Negative One"
+            if (string.IsNullOrEmpty(_input))
+            {
+                return "Negative One";
+            }
+
+            // If the input string is not null or empty, sum the ASCII values
+            // of the characters
+            foreach (char c in _input)
+            {
+                sum += (int)c;
+            }
+
+            // Reassign the sum to a string
+            string sumStr = sum.ToString();
+            string words = "";
+
+            // Look at each character in the sum string and assign the
+            // corresponding word to the words string
+            foreach (char c in sumStr)
+            {
+                switch (c)
+                {
+                    case '0':
+                        words += "Zero ";
+                        break;
+                    case '1':
+                        words += "One ";
+                        break;
+                    case '2':
+                        words += "Two ";
+                        break;
+                    case '3':
+                        words += "Three ";
+                        break;
+                    case '4':
+                        words += "Four ";
+                        break;
+                    case '5':
+                        words += "Five ";
+                        break;
+                    case '6':
+                        words += "Six ";
+                        break;
+                    case '7':
+                        words += "Seven ";
+                        break;
+                    case '8':
+                        words += "Eight ";
+                        break;
+                    case '9':
+                        words += "Nine ";
+                        break;
+                }
+            }
+            // Return the sum of ASCII values as a string of words
+            return words;
+        }
+
+        // Override the Equals method to compare the current data input to the class with a literal or variable string
+        public override bool Equals(object? obj)
+        {
+            //if obj is not a string, return false
+            if (!(obj is string))
+                return false;
+            if (obj == null) return false;
+            //if obj is a string, compare it to the current data input
+            return obj.ToString() == _input;
+
+        }
+    }
+}
